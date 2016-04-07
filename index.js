4c51724878d5eaa1f6f38fc2d952bf93ed87b6a9
timqian <timqian92@qq.com>
2016-04-07 16:15:57 +0800

    4-7

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 // generate json file
 
+import fs from 'fs'
+import moment from 'moment'
+
 import getHN from './src/getHN'
 import getReddit from './src/getReddit'
 import getMedium from './src/getMedium'
 import getV2 from './src/getV2'
 import getGithub from './src/getGithub'
-import fs from 'fs'
+
 
 async function makeJson() {
   const json = {};
-  json.v2ex = await getV2().catch(err => {throw err});
-  console.log('v2ex done');
-  json.medium = await getMedium().catch(err => {throw err});
-  console.log('medium done'); 
-  json.github = await getGithub().catch(err => {throw err}); 
-  console.log('github done');
   json.HN = await getHN().catch(err => {throw err});   
   console.log('HN done');
+  json.v2ex = await getV2().catch(err => {throw err});
+  console.log('v2ex done');
+  json.github = await getGithub().catch(err => {throw err}); 
+  console.log('github done');
   json.reddit = await getReddit().catch(err => {throw err});
   console.log('reddit done');
+  json.medium = await getMedium().catch(err => {throw err});
+  console.log('medium done'); 
+  console.log(json);
   return json;
 }
 
+
 makeJson()
   .then(json => {
-    console.log(json);
-    const d = new Date();
-    const fileName = `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()}.json`
+    const fileName = `${moment().format('YYYY-MM-DD')}.json`
     fs.writeFile(`./data/${fileName}`, JSON.stringify(json), 'utf8', (err) => {
       if (err) throw err;
       console.log(fileName, 'wrote succeed');
