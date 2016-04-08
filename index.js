c1eb3a433ba2dd2db053fa4cc72aa65eb7fa01be
timqian <timqian92@qq.com>
2016-04-08 12:11:06 +0800

    4-8

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ async function makeJson() {
   const json = {};
   json.HN = await getHN().catch(err => {throw err});   
   console.log('HN done');
-  json.v2ex = await getV2().catch(err => {throw err});
-  console.log('v2ex done');
   json.github = await getGithub().catch(err => {throw err}); 
   console.log('github done');
   json.reddit = await getReddit().catch(err => {throw err});
   console.log('reddit done');
   json.medium = await getMedium().catch(err => {throw err});
   console.log('medium done'); 
+  json.v2ex = await getV2().catch(err => {throw err});
+  console.log('v2ex done');
   console.log(json);
   return json;
 }
