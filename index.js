c6ae8aacbccaa628417f6f533a3cdf3af3912f02
timqian <timqian92@qq.com>
2016-06-06 21:57:48 +0800

    add json

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ import moment from 'moment'
 import getDailyHeadline from './src/getDailyHeadline'
 
 (async () => {
+
+  // generate daily headline json file
   const dailyHeadline = await getDailyHeadline()
   fs.writeFileSync(`./data/${moment().format('YYYY/MM/DD')}.json`, JSON.stringify(dailyHeadline), 'utf8')
   console.log('write dailyHeadline succeed')
+
+
+  // // generate weekly headline json file
+  // if (/*周一*/) {
+  //
+  // }
+  //
+  // // generate monthly headline json file
+  // if (/*月一*/) {
+  //
+  // }
 
 })()
