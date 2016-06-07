1cd49fe1359b523ae0ac4f531aaf49bba31ba3e0
timqian <timqian92@qq.com>
2016-06-07 17:50:03 +0800

    weeklyHeadline

--- scripts/generateWeekHeadlines.js
+++ scripts/generateWeekHeadlines.js
@@ -19,13 +19,16 @@ for (let i = 0; i < 6; i++) {
 
 (async () => {
   for (let sunday of dayArray) {
+    const weekOfMonth = moment(sunday).day(-7).week() - moment(sunday).day(-7).startOf('month').week() + 1
+    const fileName = `./data/${moment(sunday).day(-1).format('YYYY/MM')}/w${weekOfMonth}.json`
+    console.log('fileName: ', fileName)
+
     const fromDate = moment(sunday).day(-7).format('YYYY/MM/DD')
     const toDate = moment(sunday).day(-1).format('YYYY/MM/DD')
     console.log(`from ${fromDate} to ${toDate}`)
     const weeklyHeadline = await getAllHeadlineOfRange(fromDate, toDate, 10)
     console.log('weeklyHeadline: ', weeklyHeadline)
-    const weekOfMonth = moment(sunday).day(-1).week() - moment(sunday).startOf('month').week() + 1
-    const fileName = `./data/${moment(sunday).day(-1).format('YYYY/MM')}/w${weekOfMonth}.json`
+
     fs.writeFileSync(fileName, JSON.stringify(weeklyHeadline), 'utf8')
     console.log('weeklyHeadline wrote succeed')
   }
