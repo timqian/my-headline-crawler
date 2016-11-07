b63c7c8bde5c08d25c510a2691a154cfdd6a0191
timqian <timqian92@qq.com>
2016-11-07 21:49:19 +0800

    fix weekly monthly

--- src/getAllHeadlineOfRange.js
+++ src/getAllHeadlineOfRange.js
@@ -1,13 +1,13 @@
 import getHeadlineOfRange from './getHeadlineOfRange'
 
 export default async function (from, to, limit) {
-  const [HN, reddit, medium, productHunt, v2ex] = await Promise.all([
+  const [HN, medium, productHunt, v2ex] = await Promise.all([
     getHeadlineOfRange('HN', from, to, limit),
-    getHeadlineOfRange('reddit', from, to, limit),
+    // getHeadlineOfRange('reddit', from, to, limit),
     getHeadlineOfRange('medium', from, to, limit),
     getHeadlineOfRange('productHunt', from, to, limit),
     getHeadlineOfRange('v2ex', from, to, limit)
   ])
 
-  return {HN, reddit, medium, v2ex, productHunt}
+  return {HN, medium, v2ex, productHunt}
 }
